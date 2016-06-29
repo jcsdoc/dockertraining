7b5ab10d865486088f2added4f67ace6e94e2c12
jcsdoc <jcsdoc@hotmail.com>
2016-06-29 11:58:15 +0000

    lets try again

--- src/ihate.java
+++ src/ihate.java
@@ -1,5 +1,5 @@
 public class ihate {
 	public static void main (String [] args) {
-		System.out.println("I hate java");
+		System.out.println("I hate java ... that's how we rooooooollllllll");
 	}
 }
